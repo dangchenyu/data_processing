ab5c324b137bfffeca7f51e1402486a8b1a8f0ce
dcy0921@outlook.com <dcy0921@outlook.com>
2020-01-10 16:42:44 +0800

    new version

--- create_new_json.py
+++ create_new_json.py
@@ -6,7 +6,7 @@ def main(json_path):
     with open(json_path) as json_file:
         anno=json.load(json_file)
         for anno_img in anno:
-            image_name='2_'+anno_img['image_name']
+            image_name=anno_img['image_name']
             for item in anno_img['annotations'][0]['anno']:
                 new_dict={}
                 new_dict['image_name']=image_name
@@ -14,8 +14,8 @@ def main(json_path):
                 json_list.append(new_dict)
         str=json.dumps(json_list)
     json_file.close()
-    new_json= open('D:\\PycharmProjects\\deep-high-resolution-net.pytorch\\data\\cabin\\annot\\temp_back.json','w+')
+    new_json= open('D:\\PycharmProjects\\deep-high-resolution-net.pytorch\\data\\cabin\\annot\\temp.json','w+')
     new_json.write(str)
     new_json.close()
 if __name__ == '__main__':
-    main('D:\\PycharmProjects\\deep-high-resolution-net.pytorch\\data\\cabin\\annot\\#5108back.json')
+    main('D:\\PycharmProjects\\deep-high-resolution-net.pytorch\\data\\cabin\\annot\\#5130.json')
